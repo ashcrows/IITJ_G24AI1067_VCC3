73df5d5418d7185ee9dddde2b9420f5444570904
ASHISH SINHA <ashcrows@users.noreply.github.com>
2025-03-12 03:36:16 +0530

    Updated app code

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+import socket
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello from GCP VM"
+    host_ip = request.host.split(':')[0]  # Get IP from the request
+    host_name = socket.gethostname()      # Get hostname
+    return f"Hello from VM running on {host_name} (IP: {host_ip})"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
